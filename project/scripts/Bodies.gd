ff56feec70f08a0b95908cf61b2d9bc74a2f51cb
Lucas Mattioli <lucas.mattioli@live.com>
2018-06-22 14:43:07 -0300

    Update body movemnet.

--- project/scripts/Bodies.gd
+++ project/scripts/Bodies.gd
@@ -70,23 +70,29 @@ func update_athlete(data):
 		transform = transform.rotated(Y, -data.ul[0])
 		athlete_skeleton.set_bone_pose(LEG_UR, transform)
 
-		# # We should not do anything on the X axis
-		# transform = athlete_skeleton.get_bone_pose(LEG_LL)
-		# transform = transform.rotated(Z, -data.ll[1])
-		# transform = transform.rotated(Y, -data.ll[0])
-		# athlete_skeleton.set_bone_pose(LEG_LL, transform)
-		#
-		# # We should not do anything on the X axis
-		# transform = athlete_skeleton.get_bone_pose(LEG_LR)
-		# transform = transform.rotated(Z, -data.ll[1])
-		# transform = transform.rotated(Y, -data.ll[0])
-		# athlete_skeleton.set_bone_pose(LEG_LR, transform)
+		# We should not do anything on the X axis
+		transform = athlete_skeleton.get_bone_pose(LEG_LL)
+		transform = transform.rotated(Z, -data.ul[1])
+		if data.ul[0] < 0.0 or data.ul[0] > 5.0:
+			transform = transform.rotated(Y, -data.ul[0])
+		else:
+			transform = transform.rotated(Y, data.ul[0]*2)
+		athlete_skeleton.set_bone_pose(LEG_LL, transform)
+		
+		# We should not do anything on the X axis
+		transform = athlete_skeleton.get_bone_pose(LEG_LR)
+		transform = transform.rotated(Z, -data.ul[1])
+		if data.ul[0] < 0.0 or data.ul[0] > 5.0:
+			transform = transform.rotated(Y, -data.ul[0])
+		else:
+			transform = transform.rotated(Y, data.ul[0]*2)
+		athlete_skeleton.set_bone_pose(LEG_LR, transform)
 
 		calculate_speed(data.ul[0], OS.get_ticks_msec())
 	pass
 
 func set_athlete_frame(seconds):
-	athlete_animation.play("default")
+	athlete_animation.play("ArmatureAction")
 	athlete_animation.seek(seconds, true)
 	athlete_animation.stop()
 	pass
