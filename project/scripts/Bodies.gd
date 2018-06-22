582986f3821a96bf65e460ad90528aa35f0576b4
Jonathan Moraes <arkyebr@gmail.com>
2018-06-22 16:48:11 -0300

    Offset

--- project/scripts/Bodies.gd
+++ project/scripts/Bodies.gd
@@ -51,54 +51,47 @@ func update_ideal_animation(data):
 func update_athlete(data):
 	if data.state == data.IN_ACTIVITY:
 		var value = data.ul[0]
-		if value < 0.0 or value > 5.0:
-			value = 0.1
-		elif value > 1.0:
-			value = 0.9
+		if value < -0.2:
+			value = -0.2
+		elif value > 1.2:
+			value = 1.2
 		var frame = value*2.5
 		set_athlete_frame(frame)
 
 		# We should not do anything on the X axis
 		var transform = athlete_skeleton.get_bone_pose(LEG_UL)
 		transform = transform.rotated(Z, -data.ul[1])
-		transform = transform.rotated(Y, -data.ul[0])
+		transform = transform.rotated(Y, -value)
 		athlete_skeleton.set_bone_pose(LEG_UL, transform)
 
 		# We should not do anything on the X axis
 		transform = athlete_skeleton.get_bone_pose(LEG_UR)
 		transform = transform.rotated(Z, -data.ul[1])
-		transform = transform.rotated(Y, -data.ul[0])
+		transform = transform.rotated(Y, -value)
 		athlete_skeleton.set_bone_pose(LEG_UR, transform)
 
 		# We should not do anything on the X axis
 		transform = athlete_skeleton.get_bone_pose(LEG_LL)
 		transform = transform.rotated(Z, -data.ul[1])
-		if data.ul[0] < 0.0 or data.ul[0] > 5.0:
-			transform = transform.rotated(Y, -data.ul[0])
-		else:
-			transform = transform.rotated(Y, data.ul[0]*2)
+		transform = transform.rotated(Y, value*2)
 		athlete_skeleton.set_bone_pose(LEG_LL, transform)
-		
+
 		# We should not do anything on the X axis
 		transform = athlete_skeleton.get_bone_pose(LEG_LR)
-		transform = transform.rotated(Z, -data.ul[1])
-		if data.ul[0] < 0.0 or data.ul[0] > 5.0:
-			transform = transform.rotated(Y, -data.ul[0])
-		else:
-			transform = transform.rotated(Y, data.ul[0]*2)
+		transform = transform.rotated(Y, value*2)
 		athlete_skeleton.set_bone_pose(LEG_LR, transform)
 
 		calculate_speed(data.ul[0], OS.get_ticks_msec())
 	pass
 
 func set_athlete_frame(seconds):
-	athlete_animation.play("ArmatureAction")
+	athlete_animation.play("default")
 	athlete_animation.seek(seconds, true)
 	athlete_animation.stop()
 	pass
 
 func calculate_speed(y, ms):
-	if not speed_data.base and y < 0.1:
+	if not speed_data.base and y < 0.3:
 		speed_data.base = true
 		if speed_data.summit:
 			speed_data.base = false
@@ -106,7 +99,7 @@ func calculate_speed(y, ms):
 			speed_data.current = int(60000/(ms - speed_data.start_time))
 		else:
 			speed_data.start_time = ms
-	elif not speed_data.summit and speed_data.base and y > 0.9 and y < 5.0:
+	elif not speed_data.summit and speed_data.base and y > 0.7 and y < 2.0:
 		speed_data.summit = true
 		speed_data.base = false
 	pass
--- project/scripts/ServerRequester.gd
+++ project/scripts/ServerRequester.gd
@@ -5,6 +5,7 @@ class Dataset:
 	var ll = [0.0, 0.0, 0.0]
 	var state = 0
 	var force = 0
+	# var force = 120
 	var speed = 0
 	var timer = 0
 	var difficulty = 0
@@ -12,8 +13,8 @@ class Dataset:
 	const WAITING = 1
 	const IN_ACTIVITY = 2
 
-# const URL = "http://localhost:5000/"
-const URL = "http://192.168.1.101:5000/"
+const URL = "http://localhost:5000/"
+# const URL = "http://192.168.1.101:5000/"
 
 var data = Dataset.new()
 var mock = [0.0, 0.0, 0.0]
@@ -36,13 +37,23 @@ func extract_info(result):
 		data.state = result.state
 		if result.state == data.IN_ACTIVITY:
 			data.ul = result.ul
+			# data.ul = update_mock()
 			# data.ll = result.ll
 			data.difficulty = result.difficulty
 			data.force = result.force
 			data.timer = result.timer
 	else:
 		data.state = data.NO_RESPONSE
+	# data.state = data.IN_ACTIVITY
 	pass
+
+# func update_mock():
+# 	if mock[0] < -0.5:
+# 		mock_signal = 1
+# 	elif mock[0] > 0.7:
+# 		mock_signal = -1
+# 	mock[0] += 0.2*mock_signal
+# 	return mock
 
 func _on_RequestTimer_timeout():
 	$InfoEndpoint.perform_request()
@@ -53,4 +64,6 @@ func _on_InfoEndpoint_request_completed(result, response_code, headers, body):
 		extract_info(JSON.parse(body.get_string_from_utf8()).result)
 	else:
 		data.state = data.NO_RESPONSE
+		# data.ul = update_mock()
+		# data.state = data.IN_ACTIVITY
 	pass
